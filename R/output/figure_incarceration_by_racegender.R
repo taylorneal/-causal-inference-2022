be2fecb0caaf8417ee327a8e9429e7c431376064
Taylor Neal <taylor.neal@utexas.edu>
2022-02-18 01:50:34 -0600

    correcting new incarceration plot script.

--- R/output/figure_incarceration_by_racegender.R
+++ R/output/figure_incarceration_by_racegender.R
@@ -3,15 +3,15 @@
 
 read_csv(here("data/NLSY97_clean.csv")) %>%
   group_by(race, gender) %>%
-  summarize(total_arrests = mean(total_arrests)) %>%
-  ggplot(aes(race, total_arrests, fill = gender)) +
+  summarize(months_incarcerated = mean(months_incarcerated)) %>%
+  ggplot(aes(race, months_incarcerated, fill = gender)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(
     x = "Race", 
-    y = "Mean Arrests", 
+    y = "Mean Months Incarcerated", 
     fill = "Gender",
-    title = "Mean Number of Arrests in 2002 by Race and Gender") +
+    title = "Mean Months Incarcerated in 2002 by Race and Gender") +
   theme_minimal() +
   scale_fill_economist()
 
-ggsave(here("figures/arrests_by_racegender.png"), width=8, height=4.5)
+ggsave(here("figures/incarceration_by_racegender.png"), width=8, height=4.5)
