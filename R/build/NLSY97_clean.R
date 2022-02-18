45c04addb04331b56c66fcbaed2f831ce1d540a3
Taylor Neal <taylor.neal@utexas.edu>
2022-02-18 01:30:04 -0600

    Updated build script for incarceration data.

--- R/build/NLSY97_clean.R
+++ R/build/NLSY97_clean.R
@@ -18,7 +18,7 @@ read_csv(here("data/NLSY97_raw.csv")) %>%
   
   # sum across the months using rowwise
   rowwise() %>%
-  mutate(total_arrests = sum(c_across(starts_with("E")), na.rm = TRUE)) %>%
+  mutate(months_incarcerated = sum(c_across(starts_with("E")), na.rm = TRUE)) %>%
   ungroup() %>%
   
   # recode the gender variable
@@ -33,7 +33,7 @@ read_csv(here("data/NLSY97_raw.csv")) %>%
   )) %>%
   
   # finally, select the variables that will be used in the analysis
-  select(race, gender, total_arrests) %>%
+  select(race, gender, months_incarcerated) %>%
   
   # write to a csv
   write_csv(here("data/NLSY97_clean.csv"))
